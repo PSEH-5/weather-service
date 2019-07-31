ae46eca8b45f94c95153ad16d993c96a53aadc3f
PSEH-5 <PSEH-5@sapient.com>
2019-07-31 14:30:24 +0530

    improved version

--- src/main/java/com/sapient/weather/predicator/weatherservice/WeatherService.java
+++ src/main/java/com/sapient/weather/predicator/weatherservice/WeatherService.java
@@ -3,6 +3,7 @@ package com.sapient.weather.predicator.weatherservice;
 
 import com.sapient.weather.predicator.model.Response;
 import com.sapient.weather.predicator.model.Wresponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,15 +11,22 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Service("weatherService")
+@Slf4j
 public class WeatherService {
 
 
+
+
+    public static final String HTTPS_SAMPLES_OPENWEATHERMAP_ORG_DATA_2_5_FORECAST_Q = "https://samples.openweathermap.org/data/2.5/forecast?q=";
+    public static final String RAIN = "Rain";
+    public static final String USE_SUNSCREEN_LOTION = "Use sunscreen lotion";
+    public static final String CARRY_UMBRELLA = "Carry Umbrella";
+
     public Wresponse getCityWeahter(String cityName) {
 
-        Wresponse whetherReport = null;
         try {
             RestTemplate restTemplate = new RestTemplate();
-            final String baseUrl = "https://samples.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&&appid=b6907d289e10d714a6e88b30761fae22";
+            final String baseUrl = HTTPS_SAMPLES_OPENWEATHERMAP_ORG_DATA_2_5_FORECAST_Q + cityName + ",us&&appid=b6907d289e10d714a6e88b30761fae22";
             URI uri = new URI(baseUrl);
 
             Response response = restTemplate.getForObject(uri, Response.class);
@@ -26,30 +34,28 @@ public class WeatherService {
             java.util.List<com.sapient.weather.predicator.model.List> mains = response.getList();
 
             for (int i = 0; i < 3; i++) {
-                if (mains.get(i).getWeather().get(0).getDescription().contains("Rain")) {
-                    return new Wresponse("Carry Umbrella");
+                if (mains.get(i).getWeather().get(0).getDescription().contains(RAIN)) {
+                    return new Wresponse(CARRY_UMBRELLA);
                 }
             }
 
             Float minTemp = Float.MAX_VALUE;
             Float maxTemp = Float.MIN_VALUE;
 
-            Double avgTemp = mains.stream().limit(3).mapToDouble(e -> e.getMain().getTemp()).sum() / 3;
+            Double avgTemp = 0.0;
             for (int i = 0; i < 3; i++) {
-                if (mains.get(i).getMain().getTempMin() < minTemp) {
-                    minTemp = mains.get(i).getMain().getTempMin();
+                Float tempValue = mains.get(i).getMain().getTempMin();
+                if (tempValue < minTemp) {
+                    minTemp = tempValue;
                 }
+                if (tempValue > maxTemp) {
+                    maxTemp = tempValue;
+                }
+                avgTemp += tempValue;
             }
 
-            for (int i = 0; i < 3; i++) {
-                if (mains.get(i).getMain().getTempMin() > maxTemp) {
-                    maxTemp = mains.get(i).getMain().getTempMin();
-                }
-            }
-
-
-            if (avgTemp >= 313.15) {
-                return new Wresponse("Use sunscreen lotion");
+            if (avgTemp/3 >= 313.15) {
+                return new Wresponse(USE_SUNSCREEN_LOTION);
             }
 
 
@@ -57,7 +63,7 @@ public class WeatherService {
 
 
         } catch (URISyntaxException e) {
-            e.printStackTrace();
+           log.error(" error is raised while calling rest api");
         }
         return null;
     }
